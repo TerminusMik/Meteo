92544280b97418e54219faf52eb3143b33f04a84
TerminusMik <79770524+TerminusMik@users.noreply.github.com>
2021-04-05 17:21:51 +0200

    Add files via upload

---
+++ Tms-Météo/__resource.lua
@@ -0,0 +1,39 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Petit menu pour controler la meteo fait par Terminus-Mik#4576 '
+
+
+
+client_scripts {
+    "src/RageUI/client/RMenu.lua",
+    "src/RageUI/client/menu/RageUI.lua",
+    "src/RageUI/client/menu/Menu.lua",
+    "src/RageUI/client/menu/MenuController.lua",
+    "src/RageUI/client/components/*.lua",
+    "src/RageUI/client/menu/elements/*.lua",
+    "src/RageUI/client/menu/items/*.lua",
+    "src/RageUI/client/menu/panels/*.lua",
+    "src/RageUI/client/menu/windows/*.lua",
+}
+
+server_scripts {
+	"@mysql-async/lib/MySQL.lua",
+	'@es_extended/locale.lua',
+	"server.lua",
+
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	
+	"client.lua",
+	"menu.lua"
+
+
+
+}
+
+
+
+
+
